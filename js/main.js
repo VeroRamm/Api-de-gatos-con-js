cf6df58c43fb0b2271ba0312c26b44d35cbfe5a9
Veronica Trias <vero.ramm@gmail.com>
2022-06-09 11:28:30 -0300

    Se modifico el js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const gato = new Vue({
+/*const gato = new Vue({
     el: '#gatos',
     data: {
         gatos: [],
@@ -19,7 +19,45 @@ const gato = new Vue({
 });
 
 
-// Language: javascript
+// Language: javascript*/
+const API_URL = 'https://raw.githubusercontent.com/juan-pinzon/gatosJSON/master/cats.json';
 
+const HTMLResponse = document.querySelector('#cats');
+const tpl = document.createElement('template');
 
-    
+fetch(API_URL)
+    .then(response => response.json())
+    .then(data => {
+        tpl.innerHTML = `
+            <ul>
+                ${data.Cats.map(cat => `
+                    <li>
+                        <img src="${cat.Image}">
+                        <h3>${cat.Name}</h3>
+                        <p>${cat.Description}</p>
+                        <p>${cat.WebSite}</p>
+                    </li>
+                `).join('')}
+            </ul>
+        `;
+        HTMLResponse.appendChild(tpl.content);
+    });
+
+const buscar = document.querySelector('#buscar');
+const boton = document.querySelector('#boton');
+
+const filtrar = () => {
+       // console.log(buscar.value);
+       const texto = buscar.value.toLowerCase();
+         const lista = document.querySelectorAll('li');
+            lista.forEach(element => {
+                const textoLi = element.textContent.toLowerCase();
+                if(textoLi.includes(texto)){
+                    element.style.display = 'block';
+                } else {
+                    element.style.display = 'none';
+                }
+            });
+}
+
+boton.addEventListener('click', filtrar);
